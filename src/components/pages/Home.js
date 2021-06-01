079b2275b137f503dfbcde1baf06ff27738f1f7d
Renato Soares <renatosoarespro@hotmail.com>
2021-06-01 16:09:31 -0300

    fetch content

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import {
   FaChargingStation,
@@ -20,110 +20,150 @@ import Footer from "components/organisms/Footer";
 import ProductGrid from "components/organisms/ProductGrid";
 import Accordion, { AccordionGroup } from "components/atoms/Accordion";
 
-import BirdImage from "assets/bg-bird.jpg";
 import CameraVideo from "assets/camera.mp4";
 
-const Home = ({ products }) => (
-  <>
-    <Hero image={BirdImage}>
-      <Heading>
-        <h1>
-          Dolor sit amet <strong>consectetur</strong>
-          <br />
-          adipisicing elitit
-        </h1>
-      </Heading>
-      <ul>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-        <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
-      </ul>
-      <Button color="primary" variant="outlined">
-        Mostre-se agora
-      </Button>
-    </Hero>
-    <Section>
-      <Grid sm={2} md={4}>
-        <Feature
-          icon={<FaChargingStation />}
-          title="Adipisicing elit, consectetur"
-        >
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaCameraRetro />} title="Ipsum dolor sit consectetur">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaImage />} title="Ipsum dolor">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        </Feature>
-        <Feature icon={<FaUserClock />} title="Lorem ipsum dolor sit">
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-        </Feature>
-      </Grid>
-    </Section>
-    <Section inverse>
-      <Heading>
-        <h2>Conheça nossos serviços</h2>
-      </Heading>
-      <ProductGrid products={products} />
-    </Section>
-    <Section>
-      <Grid md={2}>
-        <div>
-          <Heading>
-            <h2>Photography auto click</h2>
-          </Heading>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
-            totam eius autem esse cumque neque vero enim quisquam iure nulla
-            delectus dolore facere, nam labore, ipsam quae omnis amet ad.{" "}
-          </p>
+const Home = ({ products }) => {
+  const [highlightPost, setHighlightPost] = useState({});
+  const [highlightImage, setHighlightImage] = useState("");
+
+  useEffect(() => {
+    const myHeaders = new Headers();
+
+    const myInit = {
+      method: "GET",
+      headers: myHeaders,
+      mode: "cors",
+      cache: "default",
+    };
+
+    const myRequest = new Request(
+      "//canindesoares.com/wp-json/wp/v2/posts/?categories=1&per_page=1",
+      myInit
+    );
+
+    fetch(myRequest)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("fetch post", data);
+        setHighlightPost(data.pop());
+      });
+  }, []);
+
+  useEffect(() => {
+    if (Object.entries(highlightPost).length > 0) {
+      fetch(
+        `//canindesoares.com/wp-json/wp/v2/media/${highlightPost.featured_media}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("fetch media", data);
+          setHighlightImage(data);
+        });
+    }
+  }, [highlightPost]);
+
+  return (
+    <>
+      <Hero image={highlightImage.source_url}>
+        <Heading>
+          <h1>
+            Dolor sit amet <strong>consectetur</strong>
+            <br />
+            adipisicing elitit
+          </h1>
+        </Heading>
+        <ul>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+          <li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+        </ul>
+        <Button color="primary" variant="outlined">
+          Mostre-se agora
+        </Button>
+      </Hero>
+      <Section>
+        <Grid sm={2} md={4}>
+          <Feature
+            icon={<FaChargingStation />}
+            title="Adipisicing elit, consectetur"
+          >
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Feature>
+          <Feature icon={<FaCameraRetro />} title="Ipsum dolor sit consectetur">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Feature>
+          <Feature icon={<FaImage />} title="Ipsum dolor">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Feature>
+          <Feature icon={<FaUserClock />} title="Lorem ipsum dolor sit">
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          </Feature>
+        </Grid>
+      </Section>
+      <Section inverse>
+        <Heading>
+          <h2>Conheça nossos serviços</h2>
+        </Heading>
+        <ProductGrid products={products} />
+      </Section>
+      <Section>
+        <Grid md={2}>
           <div>
-            <Button as={Link} to="/about" color="primary">
-              Know more
-            </Button>
+            <Heading>
+              <h2>Photography auto click</h2>
+            </Heading>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+              totam eius autem esse cumque neque vero enim quisquam iure nulla
+              delectus dolore facere, nam labore, ipsam quae omnis amet ad.{" "}
+            </p>
+            <div>
+              <Button as={Link} to="/about" color="primary">
+                Know more
+              </Button>
+            </div>
           </div>
-        </div>
-        <div>
-          <video
-            src={CameraVideo}
-            width="100%"
-            autoPlay
-            playsInline
-            loop
-            muted
-          />
-        </div>
-      </Grid>
-    </Section>
-    <Section inverse>
-      <Heading>
-        <h2>Dúvidas recorrentes</h2>
-      </Heading>
-      <AccordionGroup>
-        <Accordion title="How do I use flash during the day?">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-          sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
-          corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
-          nostrum ipsa at!
-        </Accordion>
-        <Accordion title="What is the best portrait lens?">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-          sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
-          corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
-          nostrum ipsa at!
-        </Accordion>
-        <Accordion title="Transporting machinery how do I do it?">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
-          sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
-          corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
-          nostrum ipsa at!
-        </Accordion>
-      </AccordionGroup>
-    </Section>
-    <Footer />
-  </>
-);
+          <div>
+            <video
+              src={CameraVideo}
+              width="100%"
+              autoPlay
+              playsInline
+              loop
+              muted
+            />
+          </div>
+        </Grid>
+      </Section>
+      <Section inverse>
+        <Heading>
+          <h2>Dúvidas recorrentes</h2>
+        </Heading>
+        <AccordionGroup>
+          <Accordion title="How do I use flash during the day?">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
+            corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
+            nostrum ipsa at!
+          </Accordion>
+          <Accordion title="What is the best portrait lens?">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
+            corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
+            nostrum ipsa at!
+          </Accordion>
+          <Accordion title="Transporting machinery how do I do it?">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores,
+            sunt. Tempora cumque provident et unde maxime nam, deserunt iusto
+            corporis, porro obcaecati neque, nulla reprehenderit quaerat quia
+            nostrum ipsa at!
+          </Accordion>
+        </AccordionGroup>
+      </Section>
+      <Footer />
+    </>
+  );
+};
 
 Home.defaultProps = {
   products: [],
