541a6b3afc91e744091f73de76512dd744cad169
Renato Soares <renatosoarespro@hotmail.com>
2021-07-07 08:27:51 -0300

    define projetc with typescript

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders call to action", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/mostre-se agora/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/routes/about.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "test-utils";
-
-import About from "./about";
-
-test("scrolls to top in first render", () => {
-  render(<About />);
-  expect(window.scrollTo).toBeCalledTimes(1);
-});
