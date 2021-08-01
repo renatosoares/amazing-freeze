85b610608206be43d67f54ed3592c42038fa546c
Renato Soares <renatosoarespro@hotmail.com>
2021-08-01 05:47:31 -0300

    fix gap in list posts

--- src/components/organisms/PostGrid/index.tsx
+++ src/components/organisms/PostGrid/index.tsx
@@ -23,7 +23,7 @@ const PostGrid = ({ posts }: PostGridProps) => {
 
   return (
     <>
-      <div className="row row-cols-1 row-cols-md-3">
+      <div className="row row-cols-1 row-cols-md-2 row-cols-xl-3 gy-3">
         {filteredPosts.map((post) => (
           <div key={post.id} className="col">
             <Card>
